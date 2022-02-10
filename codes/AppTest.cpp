fe331a68d8165d53e4a7d1c48a65bb7699a78a66
pixel-Teee <1173769234@qq.com>
2022-02-10 17:27:25 +0800

    add test

--- codes/AppTest.cpp
+++ codes/AppTest.cpp
@@ -29,10 +29,60 @@ namespace app
 		//数组形式 elements:元素个数
 		printf("hmget...type:%d %d %d\n", reply->type, (int)reply->elements, length);
 
+		sss = "hset user_data:1000 maxhp 2000";
+		err = redis->RedisCommand(sss.c_str());
+		reply = (redisReply*)redis->getReply();
+		if(reply == nullptr) return;
+
+		printf("hset...type:%d %d \n", reply->type, (int)reply->integer);
+
+		sss = "hget user_data:1000 maxhp";
+		err = redis->RedisCommand(sss.c_str());
+		reply = (redisReply*)redis->getReply();
+		if (reply == nullptr) return;
+
+		printf("hget...type:%d %s \n", reply->type, reply->str);
+
+		//4、string
+		sss = "set aaa 111";
+		redis->RedisCommand(sss.c_str());
+		reply = (redisReply*)redis->getReply();
+
+		printf("set...type:%d %s\n", reply->type, reply->str);
+
+		//5、获取字符串
+		sss = "get aaa";
+		redis->RedisCommand(sss.c_str());
+		reply = (redisReply*)redis->getReply();
+
+		printf("get...type:%d %s\n", reply->type, reply->str);
+
+		//6、删除key 返回 REDIS_REPLY_INTEGER 成功返回1 失败返回0
+		sss = "del user_data:1001";
+		redis->RedisCommand(sss.c_str());
+		reply = (redisReply*)redis->getReply();
+
+		printf("del...type:%d %d\n", reply->type, (int)reply->integer);
+
+		//6、设置过期时间
+		sss = "expire user_data:1000 1000";
+		redis->RedisCommand(sss.c_str());
+		reply = (redisReply*)redis->getReply();
+
+		printf("expire...type:%d %d\n", reply->type, (int)reply->integer);
+
 		//总结
 		//一、hash
 		//hmset REDIS_REPLY_STATUS reply->str("OK")
 		//hmget REDIS_REPLY_ARRAY reply->elements(数组个数) reply->element(数组)
+		//hset  REDIS_REPLY_INTEGER reply->integer
+		//hget  REDIS_REPLY_STRING reply->str / REDIS_REPLY_NIL 查询数据不存在
+		//del/expire REDIS_REPLY_INTEGER reply->integer(删除或设置成功 0 key不存在)
+		
+		//二、string
+		//set REDIS_REPLY_STATUS reply->str("OK")
+		//get REDIS_REPLY_STRING reply->str /不存在REDIS_REPLY_NIL
+
 	}
 
 	void testData()
